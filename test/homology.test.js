a30491d9629b1f6b0ea712c8fba50cd112672c7c
Eric Weitz <eric.m.weitz@gmail.com>
2021-08-11 06:26:58 -0400

    Add more tests

--- test/homology.test.js
+++ test/homology.test.js
@@ -13,7 +13,7 @@ describe('Homology.js', () => {
     global.fetch = fetch;
   });
 
-  it('should fetch orthologs from OMA', async () => {
+  it('fetches orthologs from OMA', async () => {
 
     let genes = ['MTOR'];
     let sourceOrg = 'homo sapiens';
@@ -25,7 +25,7 @@ describe('Homology.js', () => {
     expect(orthologs[0][1].location).toBe('4:148452271-148556860');
   });
 
-  it('should fetch orthologs from OrthoDB', async () => {
+  it('fetches orthologs from OrthoDB', async () => {
 
     let genes = ['NFYA'];
     let sourceOrg = 'homo sapiens';
@@ -35,5 +35,43 @@ describe('Homology.js', () => {
 
     expect(orthologs[0][0].location).toBe('6:41072974-41102403');
     expect(orthologs[0][1].location).toBe('I:11224836-11233334');
+  });
+
+  it('handles orthologs between human and mosquito', async () => {
+
+    let genes = ['MTOR'];
+    let sourceOrg = 'homo sapiens';
+    let targetOrgs = ['anopheles gambiae'];
+
+    let orthologs = await fetchOrthologs(genes, sourceOrg, targetOrgs);
+
+    // console.log('orthologs', orthologs) previously output:
+    //
+    // orthologs [
+    //   [
+    //     { name: 'MTOR', location: '1:11106535-11262551' },
+    //     { name: '3292017', location: '3R:2406092-2413890' },
+    //     { name: '1278787', location: '3R:2406092-2413890' },
+    //     { name: '4576162', location: '3R:2406092-2413890' },
+    //     { name: 'AGAP010313', location: '3R:2406092-2413890' },
+    //     { name: 'AGAP007873', location: '3R:2406092-2413890' }
+    //   ]
+    // ]
+    //
+    // But let's not be too stringent in this test, as this is mostly a test of
+    // whether OrthoDB SPARQL processing returns anything reasonable for
+    // mosquito.  OrthoDB has a nuanced form for mosquito NCBI Gene IDs.
+    expect(orthologs[0].length).toBeGreaterThanOrEqual(2)
+  });
+
+  it('fetches orthologs for multiple genes', async () => {
+
+    let genes = ['MTOR', 'BRCA1'];
+    let sourceOrg = 'homo sapiens';
+    let targetOrgs = ['mus musculus'];
+
+    let orthologs = await fetchOrthologs(genes, sourceOrg, targetOrgs);
+
+    expect(orthologs.length).toEqual(2);
   });
 });
